6e17811ca16ac767cb9084212ec543c188adf26f
MammadovKamran <kamran.97.memmedov@gmail.com>
2024-09-20 17:43:42 +0400

    header section

---
+++ client/src/app/components/header/AccountMenu.js
@@ -0,0 +1,10 @@
+/** @format */
+"use client";
+
+import React from "react";
+
+const AccountMenu = () => {
+  return <div>AccountMenu</div>;
+};
+
+export default AccountMenu;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,13 +1,15 @@
 /** @format */
 "use client";
 
-import Navbar from "./components/Navibar";
+import Navbar from "./components/header/Navbar";
 
 export default function Page() {
   return (
     <>
-      <div>
-        <Navbar />
+      <div className="w-full">
+        <div className=" min-w-[100%] flex justify-center">
+          <Navbar />
+        </div>
       </div>
     </>
   );
